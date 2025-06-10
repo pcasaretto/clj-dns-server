fb002bb17333dca717f3fee9b35fc39807c4573b
pcasaretto <paulo.casaretto@shopify.com>
2025-06-10 18:15:19 +0200

    pipeline driven echo udp server

--- src/dns_server/core.clj
+++ src/dns_server/core.clj
@@ -5,15 +5,33 @@
    [clojure.core.async :as async :refer [<!! >!! <! >!]])
   (:import (java.net DatagramPacket DatagramSocket)))
 
+
+(defn packet->map [packet]
+  {:address (.getAddress packet)
+   :port (.getPort packet)
+   :data (.getData packet)})
+
+(defn packetmap->out-packet [m]
+  (DatagramPacket. (:data m) (alength (:data m)) (:address m) (:port m)))
+
+(defn send-packet [socket]
+  (fn [packet] (.send socket packet) :ok))
+
+(defn process [socket]
+  (comp
+   (map packet->map)
+   (map packetmap->out-packet)
+   (map (send-packet socket))))
+
+
 (defn start-receiver []
   (let [close-chan (async/chan)
         packet-chan (async/chan)
-        port 2032]
-    (socket/listen packet-chan close-chan port)
-    (async/go-loop []
-      (let [packet (<! packet-chan)]
-        (println (String. (.getData packet) 0 (.getLength packet)))
-        (recur)))
+        port 2053
+        socket (socket/listen packet-chan close-chan port)
+        cb (async/chan)]
+    (async/pipeline 1 cb (process socket) packet-chan)
+    (async/go-loop [res (<! cb)] (println res) (recur (<! cb)))
     close-chan))
 
 (defstate dns-server
